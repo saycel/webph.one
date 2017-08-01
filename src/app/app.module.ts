27d38d477b17881090876c1f6988e2e2617f418d
Marcos Gutierrez <gmarcos87@gmail.com>
2017-08-01 18:04:57 -0300

    Add router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'  ;
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -7,11 +8,17 @@ import { CustomMaterialModule } from './material.module'
 
 import { AppComponent } from './app.component';
 
+export const appRoutes: Routes  = [];
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     CustomMaterialModule,
     BrowserAnimationsModule,
     BrowserModule
