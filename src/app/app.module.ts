2f50f83fd7794030fdae0932b6e183d201b2eb0c
Marcos Gutierrez <gmarcos87@gmail.com>
2017-08-01 23:02:24 -0300

    Inject new modules and router setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router'  ;
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router'  ;
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -8,7 +8,34 @@ import { CustomMaterialModule } from './material.module'
 
 import { AppComponent } from './app.component';
 
-export const appRoutes: Routes  = [];
+import { CallModule } from './call/call.module';
+import { DirectoryModule } from './directory/directory.module';
+import { ShareModule } from './share/share.module';
+
+export const appRoutes: Routes  = [
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'call'
+  },
+  {
+    path:'call',
+    loadChildren: './call/call.module#CallModule'
+  },
+  {
+    path:'directory',
+    loadChildren: './directory/directory.module#DirectoryModule'
+  },
+  {
+    path:'share',
+    loadChildren: './share/share.module#ShareModule'
+  },
+  { 
+    path: '**',
+    redirectTo: '/call',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   declarations: [
@@ -17,11 +44,18 @@ export const appRoutes: Routes  = [];
   imports: [
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
+      {
+        preloadingStrategy: PreloadAllModules,
+        useHash: true,
+        enableTracing: true // <-- debugging purposes only
+      }
     ),
     CustomMaterialModule,
     BrowserAnimationsModule,
-    BrowserModule
+    BrowserModule,
+    CallModule,
+    ShareModule,
+    DirectoryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
