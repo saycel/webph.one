9d7b6b8068b09fcde59b274f6809f087d52f23e3
Marcos Gutierrez <gmarcos87@gmail.com>
2017-08-10 08:24:49 -0300

    Make  DTMF on session accepted and use sounds service

--- src/app/call/jssip.service.ts
+++ src/app/call/jssip.service.ts
@@ -1,12 +1,14 @@
 import JsSIP from 'jssip';
 import { Subject } from 'rxjs/Subject';
+import AudioPlayer from './sounds.service';
 
 /* DEGUB */
 JsSIP.debug.enable('JsSIP:*');
 
-export class jsSipService {
+export class JsSipService {
+  private audioPlayer = AudioPlayer;
   public events: Subject<string>;
-  public status: string = 'hangup';
+  public status = 'hangup';
   private eventHandlers: object;
   private socket: any;
   private ua: any;
@@ -59,23 +61,62 @@ export class jsSipService {
 
     this.callOptions = {
       'eventHandlers'    : this.eventHandlers,
-      'mediaConstraints' : { 'audio': true, 'video': false }
+      'mediaConstraints' : { 'audio': true, 'video': false },
+      'pcConfig' : this.configuration.pcConfig || { iceServers: [] },
+      'rtcOfferConstraints' : {
+            offerToReceiveAudio : 1,
+            offerToReceiveVideo : 0
+       },
+       'sessionTimersExpires' : 120
     };
 
     this.socket = new JsSIP.WebSocketInterface('wss://rhizortc.specialstories.org:8443');
+    this.socket.via_transport = 'auto';
     this.configuration['sockets'] = [this.socket];
     this.ua = new JsSIP.UA(this.configuration);
     this.ua.start();
   }
 
   call(number) {
-      this.status = 'calling';
-      this.ua.call(number,this.callOptions);
+    this.status = 'calling';
+    //const session = this.ua.call('sip:pearllagoon@rhizortc.specialstories.org', this.callOptions);
+    const session = this.ua.call('sip:385485876@did.callwithus.com', this.callOptions);
+
+    session.on('progress', () => {
+      this.audioPlayer.play('ringback', null);
+    });
+
+    session.on('failed', (data) => {
+      this.audioPlayer.stop('ringback');
+      this.audioPlayer.play('rejected', null);
+    });
+
+    session.on('ended', () =>	{
+      this.audioPlayer.stop('ringback');
+    });
+
+    session.on('accepted', () => {
+        this.audioPlayer.stop('ringback');
+        this.audioPlayer.play('answered', null);
+        setTimeout(() => {
+        console.log('Sending tone ' + number + '#');
+
+        const tones = number + '#';
+        let dtmfSender = null;
+        if (session.connection.getSenders) {
+            dtmfSender = session.connection.getSenders()[0].dtmf;
+        } else {
+            const peerconnection = session.connection;
+            const localStream = peerconnection.getLocalStreams()[0];
+            dtmfSender = session.connection.createDTMFSender(localStream.getAudioTracks()[0]);
+        }
+        dtmfSender.insertDTMF(tones, 400, 50);
+        }, 2000);
+    });
   }
-  
+
   hangup() {
       this.status = 'hangup';
       this.ua.terminateSessions();
-
   }
 }
