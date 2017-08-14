664ceb215bb298f9526f8a15a57ec925f4dc5323
Marcos Gutierrez <gmarcos87@gmail.com>
2017-08-14 18:42:14 -0300

    Move svg icons to global state and apply service changes on call component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
+import { ToneService } from './tone.service';
+import { JsSipService } from './jssip.service';
+
+import {DomSanitizer} from '@angular/platform-browser';
+import {MdIconRegistry} from '@angular/material';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  links:any[] = [{
+  links: any[] = [{
     label: 'Call',
     link: '/call'
   },
@@ -18,4 +24,9 @@ export class AppComponent {
     label: 'Share',
     link: '/share'
   }];
+
+  constructor(iconRegistry: MdIconRegistry, sanitizer: DomSanitizer) {
+        iconRegistry.addSvgIcon('call-end', sanitizer.bypassSecurityTrustResourceUrl('assets/call-end.svg'));
+        iconRegistry.addSvgIcon('call', sanitizer.bypassSecurityTrustResourceUrl('assets/call.svg'));
+  }
 }
