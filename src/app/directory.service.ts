e5b8b4a795334e0ce16adc428ab28d0097f2fb0c
Marcos Gutierrez <gmarcos87@gmail.com>
2017-08-25 14:18:38 -0300

    Reduce size of findal bundle

--- src/app/directory.service.ts
+++ src/app/directory.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/observable/of';
 
 
 export interface DirectoryItemI {
