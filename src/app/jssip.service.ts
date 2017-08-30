c49dfbcd41399209a330e0d345bac2319a750664
Marcos Gutierrez <gmarcos87@gmail.com>
2017-08-30 13:28:33 -0300

    Change on trigger the end of the call.

--- src/app/jssip.service.ts
+++ src/app/jssip.service.ts
@@ -280,7 +280,9 @@ export class JsSipService {
     }
 
     handleHangup() {
+        //If is any tone o sound playing
+        this.toneService.stopAll();
+        audioPlayer.stopAll();
         this.state.session.terminate();
-        this.setState({session: null});
     }
 }
