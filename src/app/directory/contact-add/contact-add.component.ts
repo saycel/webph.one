e7d09bfc4c7e607239eae5f172eb5579ef988eda
Marcos Gutierrez <gmarcos87@gmail.com>
2017-09-12 10:51:27 -0300

    Add edit and delete functions to Contact view

--- src/app/directory/contact-add/contact-add.component.ts
+++ src/app/directory/contact-add/contact-add.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router, ActivatedRoute } from '@angular/router';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+import { Subject } from 'rxjs/Subject';
 
 import { DirectoryItemI } from '../../directory.service';
 import { StorageService } from '../../storage.service';
@@ -11,11 +13,13 @@ import { StorageService } from '../../storage.service';
   templateUrl: './contact-add.component.html',
   styleUrls: ['./contact-add.component.scss']
 })
-export class ContactAddComponent implements OnInit {
+export class ContactAddComponent implements OnInit, OnDestroy {
+  public action: string;
   public form = new FormGroup({
     number: new FormControl(null, Validators.required),
     title: new FormControl(null, Validators.required)
   });
+  private ngUnsubscribe: Subject<void> = new Subject<void>(); // = new Subject(); in Typescript 2.2-2.4
 
   constructor(
     private _storageService: StorageService,
@@ -24,7 +28,47 @@ export class ContactAddComponent implements OnInit {
     private _activateRoute: ActivatedRoute) { }
 
   ngOnInit() {
-     this.form.setValue(Object.assign({}, this.form.value, { number: Number(this._activateRoute.snapshot.paramMap.get('number') || '' )}));
+    this.action = this._activateRoute.snapshot.paramMap.get('action');
+    switch (this.action) {
+      case 'add':
+        this.initAdd();
+        break;
+      case 'edit':
+        this.initEdit();
+        break;
+      default:
+        this._router.navigate(['/directory']);
+     }
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
+  initAdd() {
+    this.form.setValue(Object.assign({}, this.form.value, { number: Number(this._activateRoute.snapshot.paramMap.get('number') || '' )}));
+  }
+
+  initEdit() {
+    this.form.addControl('id', new FormControl(this._activateRoute.snapshot.paramMap.get('number')));
+    this._storageService.table('contacts')
+      .read()
+      .takeUntil(this.ngUnsubscribe)
+      .map(contacts => contacts
+        .filter(contact => contact.id === this.form.value.id)
+        .reduce((a, b) => a = b, {}))
+      .subscribe(contact => {
+        this.form.setValue(Object.assign({}, this.form.value, contact));
+      });
+  }
+
+  submit() {
+    switch (this.action) {
+      case 'add': this.addContact(); break;
+      case 'edit': this.updateContact(); break;
+      default:  return false;
+    }
   }
 
   addContact() {
@@ -33,6 +77,20 @@ export class ContactAddComponent implements OnInit {
       this._router.navigate(['/call', this.form.value.number]);
       return;
     }
+  }
+
+  updateContact() {
+    if (this.form.status === 'VALID') {
+      this._storageService.table('contcats').update(this.form.value);
+      this._router.navigate(['/directory']);
+      return;
+    }
+  }
+
+  deleteContact(event) {
+    event.preventDefault();
+    this._storageService.table('contcats').delete(this.form.value);
+    this._router.navigate(['/directory']);
   }
 
   goBack() {
