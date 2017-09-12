40c910e2bce7c60d6709635ce8f2b188bcf340d9
Marcos Gutierrez <gmarcos87@gmail.com>
2017-09-12 10:49:00 -0300

    Add  direcrive to use pipe on inputs fields

--- src/app/customs-pipes/customs-pipes.module.ts
+++ src/app/customs-pipes/customs-pipes.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PhoneNumberPipe } from './phone-number.pipe';
+import { InputPhonenumberDirective } from './input-phonenumber.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    PhoneNumberPipe
+    PhoneNumberPipe,
+    InputPhonenumberDirective
   ],
   exports: [
-    PhoneNumberPipe
+    PhoneNumberPipe,
+    InputPhonenumberDirective
   ]
 })
 export class CustomsPipesModule { }
