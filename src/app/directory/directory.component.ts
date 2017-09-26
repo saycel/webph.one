f7deabf93a24028bbda333bfb156f1eb18a2eeac
Marcos Gutierrez <gmarcos87@gmail.com>
2017-09-26 08:58:41 -0300

    It prevents multiple subscriptions to the user object.

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -1,10 +1,13 @@
-import { Component, trigger, state, animate, transition, style } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Component, OnDestroy, trigger, state, animate, transition, style } from '@angular/core';
 import { Router } from '@angular/router';
 import { DirectoryService, DirectoryI, DirectoryItemI } from '../directory.service';
 import { StorageService, UserI } from '../storage.service';
 import { UserService } from '../user.service';
+
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/takeUntil';
 
 @Component({
   selector: 'app-directory',
@@ -25,13 +28,15 @@ import { UserService } from '../user.service';
   providers: [UserService]
 })
 
-export class DirectoryComponent {
+export class DirectoryComponent implements OnDestroy {
 
   public directories: Observable<DirectoryI[]>;
   public contacts: Observable<DirectoryItemI[]>;
   public contactsToggle = true;
   public directoryToggle = true;
   public user: UserI;
+    private ngUnsubscribe: Subject<void> = new Subject<void>(); // = new Subject(); in Typescript 2.2-2.4
+
 
   constructor(
     private _router: Router,
@@ -39,14 +44,25 @@ export class DirectoryComponent {
     public storageService: StorageService,
     public userService: UserService
   ) {
-    userService.userData().subscribe(
-      x => {
-        console.log(x);
-        this.user = x;
-      }
-    );
+    userService
+      .userData()
+      .takeUntil(this.ngUnsubscribe)
+      .subscribe(
+        x => {
+          this.user = x;
+        }
+      );
     this.directories = directoryService.get();
-    this.contacts = storageService.table('contacts').read().asObservable();
+    this.contacts = storageService
+      .table('contacts')
+      .read()
+      .takeUntil(this.ngUnsubscribe)
+      .asObservable();
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 
   call(item: DirectoryItemI) {
