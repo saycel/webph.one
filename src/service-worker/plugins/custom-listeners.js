e2a7b592ed5bd6a74278f998fab82b34fc5c4cd2
Marcos Gutierrez <gmarcos87@gmail.com>
2017-09-26 22:26:50 -0300

    Add notifications on incoming call

--- src/service-worker/plugins/custom-listeners.js
+++ src/service-worker/plugins/custom-listeners.js
@@ -1,11 +1,44 @@
 export function CustomListeners () {
-  return (worker) => new CustomListenersImpl(worker)
+  return (worker) => new CustomListenersImpl(worker);
 }
 
 export class CustomListenersImpl {
 
   setup (ops) {}
 
-  constructor () {}
+  constructor (worker) {
+    self.addEventListener('push', function (event) {
+        var payload = event.data.json().notification;
+
+        if ( payload.data.action === 'call-incoming' ) {
+          event.waitUntil(
+            self.registration.showNotification('Webph.one - Incoming call', {
+              body: payload.data.from,
+              vibrate: [200, 100, 200, 100, 200, 100, 400],
+              tag: 'request',
+              icon: 'assets/icons/android-chrome-192x192.png',
+              actions: [
+                { action: 'yes', title: 'Answer' },
+                { action: 'no', title: 'Hang up' }
+              ]
+            })
+          )
+        }
+    });
+
+    self.addEventListener('notificationclick', function (event) {
+      event.notification.close()
+
+      if (event.action == 'yes') {
+        event.waitUntil(
+          clients.openWindow('/#/call').then(function (windowClient) {
+          })
+        )
+      }
+    })
+
+    self.addEventListener('notificationclose', function (event) {
+    })
+  }
 
 }
