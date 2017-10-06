f82968ce97a4dded90bb352f526a252eb39e0b4a
Marcos Gutierrez <gmarcos87@gmail.com>
2017-10-06 18:51:56 -0300

    Applied modifications of modules are used and storage. Also included is a function to migrate the contents of localforge to pouchDB.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 
 import { JsSipService } from './jssip.service';
-import { DirectoryService } from './directory.service';
-import { UserService } from './user.service';
+import { DirectoryService, DirectoryItemI } from './directory.service';
+import { UserService, UserI } from './user.service';
 
 import {DomSanitizer} from '@angular/platform-browser';
 import {MdIconRegistry} from '@angular/material';
+
+// Until it is decided to remove the support
+// to the versions prior to the release.
+import * as LocalForage from 'localforage';
+import { StorageService } from './storage.service';
 
 @Component({
   selector: 'app-root',
@@ -25,11 +30,15 @@ export class AppComponent {
     public sanitizer: DomSanitizer,
     public directoryService: DirectoryService,
     private userService: UserService,
-    public jsSip: JsSipService
+    public jsSip: JsSipService,
+    public storageService: StorageService
    ) {
 
-    this.loadUser();
-    this.loadDirectory();
+    // Apply migration from the old database.
+    this.checkDB().then( () => {
+      this.loadUser();
+      this.loadDirectory();
+    });
     this.loadIcons([
       'call-end',
       'call',
@@ -59,18 +68,24 @@ export class AppComponent {
    * If they do not exist try to create one automatically.
    */
   loadUser () {
+    let connected = false;
     /** subscribe to de user data service */
     this.userService.userData().subscribe(
       () => {
         /** If the database is fully loaded and there is no user data */
-        if (this.userService.isUser() === false) {
-          /** Register user and whait for new user data*/
-          this.userService.createUser();
-        /** If the database is fully loaded and there is user data */
-        } else if (this.userService.isUser() === true) {
-          /** Start the jsSip connection */
-          this.jsSip.connect(this.userService.userData().getValue());
-        }
+        this.userService.isUser().then( status => {
+          if (status === false) {
+            /** Register user and whait for new user data*/
+            this.userService.createUser().catch(console.log);
+          /** If the database is fully loaded and there is user data */
+          } else if (status === true) {
+            /** Start the jsSip connection */
+            if (connected === false) {
+              this.jsSip.connect(this.userService.userData().getValue());
+            }
+            connected = true;
+          }
+        });
       }
     );
   }
@@ -81,5 +96,62 @@ export class AppComponent {
    */
   loadDirectory () {
     this.directoryService.get().subscribe();
+  }
+
+  /**
+   * This checks if a LocalForage db exists and
+   * migrates all the contents to PouchDB, this
+   * function will be removed in the future.
+   */
+  checkDB () {
+    return new Promise ((res, rej ) => {
+      // Get user and contacts data
+      LocalForage.config({
+        driver: LocalForage.INDEXEDDB,
+        name: 'webphoneDB'
+      });
+      const user: Promise<UserI[]> = LocalForage.getItem('user');
+      const contacts: Promise<DirectoryItemI[]> = LocalForage.getItem('contacts');
+
+      // Check save and delete
+      Promise.all([user, contacts])
+        .then((data) => {
+          // User
+          const userMigration = new Promise((resUser, rejUser) => {
+            if ( data[0] !== null ) {
+              this.storageService.table('user')
+                .read()
+                .take(1)
+                .subscribe(newUserArray => {
+                  if ( newUserArray.length ===  0 && typeof data[0][1] !== 'undefined') {
+                    console.log('DB MIGRATION - User migrated to new db');
+                    delete data[0][1].id;
+                    this.storageService.table('user').create(data[0][1]).then(resUser);
+                    LocalForage.removeItem('user');
+                  }
+                });
+            } else {
+              console.log('DB MIGRATION - Old user not found');
+              resUser();
+            }
+          });
+
+          // Contacts
+          const contactsMigrations = new Promise((resContacts, rejContacts) => {
+            if ( data[1] !== null ) {
+              data[1].map(contact => this.storageService.table('contacts').create(contact));
+              console.log('DB MIGRATION - Contacts migrated to new db');
+              LocalForage.removeItem('contacts');
+              Promise.all(data[1]).then(resContacts).catch(rejContacts);
+            } else {
+              console.log('DB MIGRATION - Old Contacts not found');
+              resContacts();
+            }
+          });
+
+          Promise.all([userMigration, contactsMigrations]).then(res).catch(rej);
+        })
+        .catch(rej);
+    });
   }
 }
