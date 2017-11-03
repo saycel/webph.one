b0093ad4fe25d5c8dec3122b2ca4510edb57b996
Marcos Gutierrez <gmarcos87@gmail.com>
2017-11-03 10:49:09 -0300

    Hotfix: Error on endoint
    This caused an error in subscription to notifications, feedback and survey.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  endpoint: 'https://webphone.rhizomatica.org/webpush/survey',
+  endpoint: 'https://webphone.rhizomatica.org/webpush/',
   kamailioNewNumber: 'https://saycel.specialstories.org/cgi-bin/allocatenumber.py'
 };
